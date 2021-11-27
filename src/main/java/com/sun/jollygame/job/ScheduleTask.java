77ed75402b5b410eb778a07e1e77755cc661ec9a
sunkai <1403417950@qq.com>
2021-11-27 13:07:26 +0800

    匹配修改1

--- src/main/java/com/sun/jollygame/job/ScheduleTask.java
+++ src/main/java/com/sun/jollygame/job/ScheduleTask.java
@@ -38,23 +38,23 @@ public class ScheduleTask {
      * 每秒执行一次心跳，扫描内存房间的用户
      */
     @Scheduled(cron = "0/5 * * * * ?")
-    public void printTest(){
+    public void printTest() {
         HeartBeatMapFactory beatMapFactory = HeartBeatMapFactory.getInstance();
 
         List<String> deleteUserList = new ArrayList<>();
-        for (Map.Entry<String, HeartBeat> entry : beatMapFactory.getHeartBeatMap().entrySet()){
-            log.info("开始发送心跳 userId:{}",entry.getKey());
+        for (Map.Entry<String, HeartBeat> entry : beatMapFactory.getHeartBeatMap().entrySet()) {
+            log.info("开始发送心跳 userId:{}", entry.getKey());
             HeartBeat heartBeat = entry.getValue();
-            log.info("heartBeat值为：{}",JSON.toJSONString(heartBeat));
+            log.info("heartBeat值为：{}", JSON.toJSONString(heartBeat));
             //调用发送接口
             this.sendHeartBeat(heartBeat.getUserId());
-            if (heartBeat.getFirstSendTime() == null){
+            if (heartBeat.getFirstSendTime() == null) {
                 heartBeat.setFirstSendTime(new Date());
                 continue;
             }
 
             Date compareTime = heartBeat.getHeartTime();
-            if (compareTime == null){
+            if (compareTime == null) {
                 compareTime = heartBeat.getFirstSendTime();
             }
             //1.判断倒计时结束，是否赢了
@@ -62,19 +62,19 @@ public class ScheduleTask {
 
             //2.判断当前是否已断线，或者，已超时
             Date now = new Date();
-            if ((now.getTime() - compareTime.getTime()) / 1000 > outTime){
+            if ((now.getTime() - compareTime.getTime()) / 1000 > outTime) {
                 //删除
                 deleteUserList.add(heartBeat.getUserId());
             }
 
             //3.判断是否已经超出对局时间
-            if ((now.getTime() - heartBeat.getFirstSendTime().getTime()) / 1000 > playTime + outTime){
+            if ((now.getTime() - heartBeat.getFirstSendTime().getTime()) / 1000 > playTime + outTime) {
                 //删除
                 deleteUserList.add(heartBeat.getUserId());
             }
         }
 
-        if (CollectionUtils.isNotEmpty(deleteUserList)){
+        if (CollectionUtils.isNotEmpty(deleteUserList)) {
             log.info("本次删除为：{}", JSON.toJSONString(deleteUserList));
             deleteUserList.forEach(x -> beatMapFactory.getHeartBeatMap().remove(x));
         }
@@ -85,13 +85,13 @@ public class ScheduleTask {
      * 每5秒生成随机图片
      */
     @Scheduled(cron = "0/5 * * * * ?")
-    public void getImgId(){
+    public void getImgId() {
         List<Integer> list = new ArrayList<Integer>();
-        for(int i=0;i<30;i++){
+        for (int i = 0; i < 30; i++) {
             list.add(i);
         }
         List<Integer> imgIdList = new ArrayList<>();
-        for(int i = 0;i<6;i++){//显示数字并将其从列表中删除,从而实现不重复.
+        for (int i = 0; i < 6; i++) {//显示数字并将其从列表中删除,从而实现不重复.
             imgIdList.add(list.remove(new Random().nextInt(list.size())));
         }
 
@@ -103,16 +103,17 @@ public class ScheduleTask {
      * 每s判断 匹配队列是否只有一人
      */
     @Scheduled(cron = "0/1 * * * * ?")
-    public void checkSingleUser(){
+    public void checkSingleUser() {
         SessionQueue sessionQueue = SessionQueue.getSessionQueue();
-        if (sessionQueue.size() == 1){
+        log.info("开始判断队列 size:{}", sessionQueue.size());
+        if (sessionQueue.size() == 1) {
             String userId = sessionQueue.consume();
             UserMapFactory mapFactory = UserMapFactory.getInstance();
             Date matchTime = mapFactory.get(userId).getMatchTime();
-            if ((matchTime.getTime() - new Date().getTime()) / 1000 > 5){
+            if ((new Date().getTime() - matchTime.getTime()) / 1000 > 5) {
                 BoolResponse boolResponse = new BoolResponse(true);
                 WebSocket.webSocketMap.get(userId).sendObjMessage(JSON.toJSONString(boolResponse));
-            }else {
+            } else {
                 try {
                     sessionQueue.produce(userId);
                 } catch (InterruptedException e) {
